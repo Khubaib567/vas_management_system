55061b1469ef15295dd6a7e27a39d012112bcf03
ahmedkhubaib2226 <ahmedkhubaib2226@gmail.com>
2025-10-07 17:18:34 +0500

    status : make db connector dynamic

--- config/db.config.js
+++ config/db.config.js
@@ -5,45 +5,59 @@ if(process.env.NODE !=="production"){
 
 // INITIZALIE THE SEQUELIZE LIBRARY FOR MAKING THE DB CONNECTION.
 const Sequelize = require("sequelize");
-// IMPORT NECCESSARY DB CREDENTIALS.
-const database = process.env.DATABASE;
-const user_name = process.env.USER_NAME;
-const password = process.env.PASSWORD;
+const  mongodbConnector   = require("../utils/mongodb.js")
 
-// DB_CLIENT INSTANCE BY USING SEQUELIZE DEFAULT OBJECT CONSTRUCTOR.
-const sequelize = new Sequelize(database, user_name, password, {
-  host: 'localhost',
-  dialect: 'mysql',
+module.exports = db_connector = async () => {
 
-});
+  try {
+      // IMPORT NECCESSARY DB CREDENTIALS.
+      const database = process.env.DATABASE;
+      const user_name = process.env.USER_NAME;
+      const password = process.env.PASSWORD;
 
-// INITIALIZE DB INSTANCE AS OBJECT WHICH CONTAINS ALL THE TABLES WITH IT'S RELATION WITH OTHER TABLES.
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-db.users = require("../model/user.model.js")(sequelize, Sequelize);
-db.services = require('../model/service.model.js')(sequelize, Sequelize);
+      // DB_CLIENT INSTANCE BY USING SEQUELIZE DEFAULT OBJECT CONSTRUCTOR.
+      const sequelize = new Sequelize(database, user_name, password, {
+        host: process.env.HOST_NAME,
+        dialect: process.env.DIALECT,
 
-db.users.hasMany(db.services,{onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-db.services.belongsTo(db.users,{onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+      });
 
+      // INITIALIZE DB INSTANCE AS OBJECT WHICH CONTAINS ALL THE TABLES WITH IT'S RELATION WITH OTHER TABLES.
+      const db = {};
+      db.Sequelize = Sequelize;
+      db.sequelize = sequelize;
+      db.users = require("../model/user.model.js")(sequelize, Sequelize);
+      db.services = require('../model/service.model.js')(sequelize, Sequelize);
 
-// CHECK THE DB CONNECTION.
-db.sequelize.sync({alter:true})
-  .then(() => {
-    console.log("Connected with database.");
-})
-  .catch((err) => {
-    console.log("Failed to Connected with database: " + err);
-});
+      db.users.hasMany(db.services,{onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+      db.services.belongsTo(db.users,{onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 
-// TO CLEAN THE DATABASE.
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
+      // CHECK THE DB CONNECTION.
+      await db.sequelize.sync({alter:true})
+      // console.log(connection)
 
-module.exports = db;
+      return db;
+      
+    
+  } catch (error) {
+    console.error('Failed to Connected with MYSQL Connector :',error.message)
+    console.log('Connecting with Mongodb Connector......')
+    mongodbConnector()
+  }
+
+
+  // TO CLEAN THE DATABASE.
+  // db.sequelize.sync({ force: true }).then(() => {
+  //   console.log("Drop and re-sync db.");
+  // });
+
+}
+
+
+db_connector()
+
+// module.exports = db;
 
 
 
