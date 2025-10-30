c8f5a6cf21a7c1dbd5255fd5a02f0b5785ae5e7a
ahmedkhubaib2226 <ahmedkhubaib2226@gmail.com>
2025-10-30 23:59:08 +0500

    status:modify cron job script

--- api/cron-job.js
+++ api/cron-job.js
@@ -9,6 +9,6 @@ export default async function handler(req, res) {
     
  } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Cron job failed" });
+    res.status(500).send({ message: "Cron job failed" });
  }
 }
