cf6c34161ca54ad83d1d4500cd305c83e0aef930
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2026-09-25 15:50:18 +0500

    status : check log for supabase database url

--- utils/postgre.connector.js
+++ utils/postgre.connector.js
@@ -16,7 +16,7 @@ module.exports = postgreSQLConnector = async () =>{
         // 1. Parse and fix the environment variable string
         // const DATABASE_URL = process.env.POSTRESQL_DATABASE_URL
         const DATABASE_URL = process.env.SUPABASE_DATABASE_URL
-        
+        console.log("DATABASE URL: " , SUPABASE_DATABASE_URL);
 
         // const dbUrl = new URL(DATABASE_URL);
         const sql = postgres(DATABASE_URL)
